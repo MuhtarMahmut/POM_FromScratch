a2cc44be31781c552250fe022207c8fe27e823ad
MuhtarMahmut <Muhtar.Mahmut0519@yahoo.com>
2019-02-25 16:21:14 -0500

    updating

--- src/test/java/Tests/YahooTest.java
+++ src/test/java/Tests/YahooTest.java
@@ -1,11 +1,7 @@
 package Tests;
 
+import Utilities.MyLibrary;
 import Utilities.TestBase;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class YahooTest extends TestBase {
@@ -15,6 +11,7 @@ public class YahooTest extends TestBase {
     public void firstTest() {
 
        driver.get("https://www.yahoo.com");
+        MyLibrary.TakeScreenshotsplease(driver,"myfirstpicture");
 
     }
 
---
+++ src/test/java/Utilities/MyLibrary.java
@@ -0,0 +1,51 @@
+package Utilities;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class MyLibrary {
+
+    public static Actions act;
+
+    public static void sleep(double a){
+        try {
+            Thread.sleep((int)(a*1000));
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+    public static WebElement DynamicElement(WebDriver driver, String tagname, String codition){
+        List<WebElement> allclickable=driver.findElements(By.tagName(tagname));
+        for(WebElement each: allclickable) {
+            if (each.getText().toLowerCase().contains(codition.toLowerCase())) {
+                return each;
+            }
+        }
+        return null;
+    }
+
+    public static void TakeScreenshotsplease(WebDriver driver, String SaveAs){
+        TakesScreenshot mirzat=(TakesScreenshot)driver;
+        File myfile=mirzat.getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(myfile, new File("src/test/captures/"+SaveAs+".jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void scrollplease(WebDriver driver, int y){
+        JavascriptExecutor js=(JavascriptExecutor)driver;
+        js.executeScript("scroll(0,"+y+")");
+
+    }
+
+
+}
